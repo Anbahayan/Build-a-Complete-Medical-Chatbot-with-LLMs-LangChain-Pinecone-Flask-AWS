368e60727638e8b0e5fbb18518cb9bf2b29612a2
Anbahayan <vickyanabhay2002@gmail.com>
2025-11-03 00:31:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+     name="medical_chatbot",
+    version="0.1.0",
+    author="Anabhayan",
+    author_email="vickyanabhay2002@gmail.com",
+    packages=find_packages(),
+    install_requires=[] 
+)
